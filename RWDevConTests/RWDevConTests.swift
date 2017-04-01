8f3feda0778368859d6c5c72b367c17e02a2ccc6
Matt Luedke <mluedke2@gmail.com>
2017-04-01 10:43:24 -0400

    random comment

--- RWDevConTests/RWDevConTests.swift
+++ RWDevConTests/RWDevConTests.swift
@@ -26,6 +26,7 @@ class RWDevConTests: XCTestCase {
     
     override func setUp() {
         super.setUp()
+      //random comment
         // Put setup code here. This method is called before the invocation of each test method in the class.
     }
     
